029356180e81b5c3c93fd2fd7afcf2ff543d03b7
Daniel-Rosa1 <57119617+Daniel-Rosa1@users.noreply.github.com>
2022-06-06 13:34:29 -0300

    finalizando o terinamentos do o cronometro e voltando a parte 4 do curso de html e css da alura

--- cronometro/cronometro.js
+++ cronometro/cronometro.js
@@ -1,32 +1,64 @@
-  let seconds = 0;
-  let millisecond = 8; 
+  let seconds = 00;
+  let millisecond = 00; 
+  let minutes = 0;
   let appendSeconds = document.getElementById("seconds")
   let appendMillisecond = document.getElementById("millisecond")
+  let appendMinutes = document.getElementById("minutes")
   let interval;
 
   function start(){
-    startTimer()
+    clearInterval(interval)
+     interval =setInterval(startTimer, 10)
+  }
+
+  function pause(){
+    clearInterval(interval)
+  }
+
+  function reset(){
+    clearInterval(interval)
+    millisecond = 0
+    seconds = 0
+    minutes = 0
+    appendMillisecond.innerHTML = addZero(millisecond)
+    appendSeconds.innerHTML = addZero(seconds)
+    appendMinutes.innerHTML = addZero(minutes)
   }
 
 
   function startTimer(){
     millisecond++
 
-    if(millisecond < 10){
-      appendMillisecond.innerHTML = "0" + millisecond
-    }else{
-      appendMillisecond.innerHTML = millisecond
-    }
+    appendMillisecond.innerHTML = addZero(millisecond)
+    appendSeconds.innerHTML = addZero(seconds)
+    appendMinutes.innerHTML = addZero(minutes)
 
-    if(seconds < 10){
-      appendSeconds.innerHTML = "0" + seconds
-    }else{
-      appendSeconds.innerHTML = seconds
-    }
-
-    if( millisecond => 99){
+    if( millisecond == 99){
       seconds++
       millisecond = 0
     } 
-    
+
+    if(seconds == 60){
+      minutes ++
+      seconds = 0
+    }
+
+    if(minutes ==60){
+      pause()
+    }
   }
+
+  function addZero(numero){
+    let retorno
+    if(numero <= 9){
+      retorno = '0'+numero
+    }
+
+    if(numero >= 10){
+      retorno = numero
+    }
+
+    return retorno
+  }
+
+
